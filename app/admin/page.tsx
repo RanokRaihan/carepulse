3c65936cb760f0b289701e54da9598dad4ea0e6b
Ranok Raihan <ranokraihan@gmail.com>
2024-08-17 11:49:50 -0400

    fixed build issue

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,28 +1,15 @@
 import { DataTable } from "@/components/DataTable";
 import StatCard from "@/components/StatCard";
-import { columns, Payment } from "@/components/table/Columns";
+import { columns } from "@/components/table/Columns";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ];
-}
-
 export default async function AdminPage() {
   const appointments = await getRecentAppointmentList();
-  const data = await getData();
+
   const { scheduledCount, pendingCount, cancelledCount, documents } =
-    appointments;
+    appointments || {};
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
